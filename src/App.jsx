190ff657f829080e9314ac5cd035ac1b8099319d
dimitriaatos <d.a.ellinas@gmail.com>
2022-10-31 19:48:35 +0200

    fix: eslint

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,15 @@ import './App.css'
 import routes from './routes'
 
 function App() {
-
 	return (
 		<div className="App">
 			<h1>music to compose music to</h1>
 			<div>listen to the music as you compose music.</div>
 			<nav>
 				<ul>
-					{
-						routes.map((route, index) => {
-							return (
-								<li key={index}>{route.name}</li>
-							)
-						})
-					}
+					{routes.map((route, index) => {
+						return <li key={index}>{route.name}</li>
+					})}
 				</ul>
 			</nav>
 		</div>
