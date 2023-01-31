165f3b70bfbdda10afd2442fc371bb019b404ae9
M1Z1 <gmizithras@gmail.com>
2023-01-31 16:40:01 +0200

    apartmentAudio tone best library

--- src/js/audio/apartment.js
+++ src/js/audio/apartment.js
@@ -1,13 +1,53 @@
 import * as Tone from 'tone'
+import { randomRange } from '@/js/audio/common'
+const { rando, randoSequence } = require('@nastyox/rando.js')
 
-const onStart = (video) => {
+let apartmentAudio = new Tone.Players([
+	'/audio/apartment/S0_03 a.mp3',
+	'/audio/apartment/S0_03 b.mp3',
+	'/audio/apartment/S0_03 c.mp3',
+	'/audio/apartment/S0_03 d.mp3',
+	'/audio/apartment/S0_03 e.mp3',
+	'/audio/apartment/S0_03 f.mp3',
+]).toDestination()
+
+apartmentAudio._buffers._buffers.forEach((_, index) => {
+	apartmentAudio.player(index).sync()
+})
+
+apartmentAudio.fadeOut = 0.3
+apartmentAudio.fadeIn = 0.08
+let a
+let id
+let apartmentAudioEvent
+Tone.Transport.debug = true
+
+const onStart = async (video) => {
+	console.log(apartmentAudio._players)
+	await Tone.start()
+	Tone.Transport.start()
 	video.current.currentTime = 0
-	video.current.play()
+	await video.current.play()
+	apartmentAudioEvent = new Tone.ToneEvent((time) => {
+		a = randomRange(0, 5)
+		apartmentAudio
+			.player(a)
+			.start(time)
+			.stop(time + video.current.duration)
+	})
+	apartmentAudioEvent.start()
+	apartmentAudioEvent.loop = true
+	console.log(Tone.now() + video.current.duration)
+	apartmentAudioEvent.loopEnd = Tone.now() + video.current.duration
 }
 
 const onStop = (video) => {
 	video.current.pause()
 	video.current.currentTime = 0
+	apartmentAudioEvent.stop()
+	apartmentAudio.stopAll()
+	Tone.Transport.stop()
+	Tone.Transport.cancel()
 }
 
 export { onStart, onStop }
--- src/js/audio/common.js
+++ src/js/audio/common.js
@@ -1,8 +1,31 @@
+const { rando, randoSequence } = require('@nastyox/rando.js')
 const randomRange = (min, max) => {
 	min = Math.ceil(min)
 	max = Math.floor(max)
 	// The maximum is inclusive and the minimum is inclusive
 	return Math.floor(Math.random() * (max - min + 1) + min)
 }
-
-export { randomRange }
+const NoRepetition = class {
+	constructor(max, waiting, reuse) {
+		const fillRange = (start, end) =>
+			Array(end - start + 1)
+				.fill(0)
+				.map((item, index) => start + index)
+		this.waiting = waiting
+		this.reuse = reuse
+		this.possible = fillRange(0, max - 1)
+		this.discarded = []
+	}
+	next() {
+		if (this.discarded.length > this.waiting || this.possible.length == 0) {
+			this.possible = this.possible.concat(this.discarded.slice(0, this.reuse))
+			this.discarded.splice(0, this.reuse)
+		}
+		const index = Math.floor(Math.random() * this.possible.length)
+		const result = this.possible[index]
+		this.possible.splice(index, 1)
+		this.discarded.push(result)
+		return result
+	}
+}
+export { randomRange, NoRepetition }
