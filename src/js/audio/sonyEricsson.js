ac644bc50b6bcd3ac2c76b826c8c53c3148c3661
M1Z1 <gmizithras@gmail.com>
2023-05-23 15:58:39 +0300

    sonyEricsson coded as menu

--- src/js/audio/sonyEricsson.js
+++ src/js/audio/sonyEricsson.js
@@ -1,58 +1,79 @@
-// import * as Tone from 'tone'
-// import Stochastic from '@/js/audio/stochastic.js'
-// import { msToS, RandomMetro } from './common'
-// import { sToMs } from './common'
-// import { randomRange } from './common'
+import * as Tone from 'tone'
+import Stochastic from '@/js/audio/stochastic.js'
+import { msToS, RandomMetro } from './common'
+import { sToMs } from './common'
+import { randomRange } from './common'
+import { randomInt, Urn } from './common'
 
-// const audioUrls = [
-// 	'/audio/sonyEricsson/S0_05 - Sony Ericsson.mp3',
-// 	'/audio/sonyEricsson/5thSentence.mp3',
-// 	'/audio/sonyEricsson/4thSentence.mp3',
-// 	'/audio/sonyEricsson/3rdSentence.mp3',
-// 	'/audio/sonyEricsson/2ndSentence.mp3',
-// 	'/audio/sonyEricsson/1stSentence.mp3',
-// ]
-// let video
-// let timingSound
-// let timingText
-// let sonyAudio = new Tone.Players(audioUrls).toDestination()
+const audioUrls = [
+	'/audio/sonyEricsson/5thSentence.mp3',
+	'/audio/sonyEricsson/4thSentence.mp3',
+	'/audio/sonyEricsson/3rdSentence.mp3',
+	'/audio/sonyEricsson/2ndSentence.mp3',
+	'/audio/sonyEricsson/1stSentence.mp3',
+	'/audio/sonyEricsson/S0_05 - Sony Ericsson.mp3',
+]
 
-// const init = (args) => {
-// 	video = args.video
-// 	video.current.loop = true
-// }
+let video
+let timingSound
+let sonyAudio = new Tone.Players(audioUrls).toDestination()
+const loaded = new Promise((resolve) => {
+	sonyAudio = new Tone.Players({
+		urls: audioUrls,
+		onload: resolve,
+	}).toDestination()
+})
 
-// let instruction1 = [{ range: [1, 5] }]
+const init = (args) => {
+	video = args.video
+	video.current.loop = true
+}
+const urn = new Urn(audioUrls.length, audioUrls.length - 2)
+let instruction1 = [
+	{
+		value: 0,
+		probability: 0.7,
+		name: 'zero',
+	},
+	{
+		value: 1,
+		probability: 0.3,
+		followedBy: ['zero'],
+	},
+]
+const coin = new Stochastic(instruction1)
 
-// let loop = new RandomMetro(() => {})
+let loopSound = new RandomMetro(() => {
+	let textOrSound = coin.next()
+	let playerIndex
+	if (textOrSound == 0) {
+		playerIndex = urn.next()
+	} else {
+		playerIndex = 5
+	}
+	let playerDuration = sonyAudio.player(playerIndex).buffer.duration
+	sonyAudio.player(playerIndex).start()
+	timingSound = playerDuration
+	return { interval: sToMs(timingSound) }
+})
 
-// const onStart = async (video) => {
-// 	await Tone.start()
-// 	timing = vacationAudio.player(0).buffer.duration
-// 	console.log('Timing' + vacationAudio.player(2).buffer.duration)
-// 	loopWaves.start({
-// 		nextInterval: () => {
-// 			return sToMs(timing)
-// 		},
-// 	})
-// 	loopText.start({
-// 		nextInterval: () => {
-// 			return sToMs(timing2)
-// 		},
-// 	})
-// 	loopOperator.start({
-// 		nextInterval: () => {
-// 			return sToMs(timingOperator)
-// 		},
-// 	})
-// 	video.current.currentTime = 0
-// 	video.current.play()
-// }
+const onStart = async (video) => {
+	await Promise.all([Tone.start(), loaded])
+	timingSound = sonyAudio.player(0).buffer.duration
+	console.log('Timing' + sonyAudio.player(2).buffer.duration)
+	loopSound.start({
+		nextInterval: () => {
+			return sToMs(timingSound)
+		},
+	})
+	video.current.currentTime = 0
+	video.current.play()
+}
 
-// const onStop = (video) => {
-// 	video.current.pause()
-// 	Tone.Transport.stop()
-// 	video.current.currentTime = 0
-// }
+const onStop = (video) => {
+	video.current.pause()
+	Tone.Transport.stop()
+	video.current.currentTime = 0
+}
 
-// export { onStart, onStop, init }
+export { onStart, onStop, init }
