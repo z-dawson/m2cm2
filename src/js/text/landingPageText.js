9c329a0be485f7d97cee71e24451463282eeb7ef
dimitriaatos <d.a.ellinas@gmail.com>
2023-10-26 01:18:33 +0300

    feat: chatbot landing page

---
+++ src/js/text/landingPageText.js
@@ -0,0 +1,25 @@
+const text = [
+	'listen to the music as you relax in your environment.',
+	'listen to the music as you attend other activities.',
+	'listen to the music as you get distracted. ',
+	'to listen to the music, click through to enter.',
+	'listen to the music while you daydream.',
+	'listen to the music as you full asleep.',
+	'listen to the music actively and attentively.',
+	'to listen to the music, click through to enter.',
+	'listen to the music passively and inattentively.',
+	'listen to the music as you compose music.',
+	'listen to the music as you think about this text.',
+	'to listen to the music, click through to enter.',
+	'listen to the music as you consider what a website is.',
+	'listen to the music and forget about everything.',
+	'listen to the music and remember who you are.',
+	'to listen to the music, click through to enter.',
+	'listen to the music and be aware of your surroundings.',
+	'listen to the music and get lost on the journey. ',
+	'listen to the music as you forget who you are. ',
+	'to listen to the music, click through to enter.',
+	'listen to the music as you lose sense of your surroundings. ',
+]
+
+export default text
