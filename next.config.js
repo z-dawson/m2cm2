8e24e51aaf04ee7ddf60b8c466740cab8a44f521
dimitriaatos <d.a.ellinas@gmail.com>
2023-11-06 22:43:40 +0200

    build and deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: false,
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+	assetPrefix = `/${repo}/`
+	basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+module.exports = {
+	assetPrefix: assetPrefix,
+	basePath: basePath,
+	reactStrictMode: false,
+}
