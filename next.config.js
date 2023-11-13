4d23b9a51ea82ae108a643a019f38db35b2d5e3f
dimitriaatos <d.a.ellinas@gmail.com>
2023-11-13 23:36:43 +0200

    fix: removing prefix

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-const hasCustomDomain = process.env.GITHUB_CUSTOM_DOMAIN || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions && !hasCustomDomain) {
-	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-	assetPrefix = `/${repo}/`
-	basePath = `/${repo}`
-}
 
 module.exports = {
-	assetPrefix: assetPrefix,
-	basePath,
 	reactStrictMode: false,
 }
